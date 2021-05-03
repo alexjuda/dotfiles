594e05993d92176e71954244d6f9280cee2482ee
Alexander Juda <alexanderjuda@gmail.com>
2021-05-03 21:46:52 +0200

    Replace <cmd> with shorthand :

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -99,18 +99,18 @@ local lsp_on_attach = function(client, buf_n)
 
     -- Keymap
     local opts = { noremap=true }
-    vim.api.nvim_buf_set_keymap(buf_n, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>ld", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>lD", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>li", ":LspInfo<cr>", opts)
-    vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>lr", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(buf_n, "n", "K", ":lua vim.lsp.buf.hover()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>ld", ":lua vim.lsp.buf.definition()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>lD", ":lua vim.lsp.buf.references()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>li", ":LspInfo<CR>", opts)
+    vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>lr", ":lua vim.lsp.buf.rename()<CR>", opts)
 
     -- Set keymap only if language server supports it. This way which-key window will show only supported stuff.
     if client.resolved_capabilities.document_formatting then
-        vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+        vim.api.nvim_buf_set_keymap(buf_n, "n", "<localleader>lf", ":lua vim.lsp.buf.formatting()<CR>", opts)
     end
     if client.resolved_capabilities.document_range_formatting then
-        vim.api.nvim_buf_set_keymap(buf_n, "v", "<localleader>lf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+        vim.api.nvim_buf_set_keymap(buf_n, "v", "<localleader>lf", ":lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
 end
 
