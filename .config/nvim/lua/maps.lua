00275ad682ce6310489f3e9610e47ee2f7d40126
Alexander Juda <alexanderjuda@gmail.com>
2021-05-03 14:42:53 +0200

    Setup vim which key

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,32 +1,50 @@
 local map = vim.api.nvim_set_keymap
-opts = { noremap = true }
+local opts = { noremap = true }
 
 
 -- Leaders
 ------------
-vim.g.mapleader = " " -- set space as the leader key
-map("n", "<Space>", "", {}) -- disable actions for space
+vim.g.mapleader = " " -- space as the leader key
+vim.g.maplocalleader = "," -- comma as the local leader key
 
+-- vim.g.which_key_map = {}
+local which_key_map = {}
+vim.fn["which_key#register"]("<Space>", "g:which_key_map")
 
--- Buffers
-------------
+-- Fire which key on leader/local leader
+map("n", "<leader>", ":WhichKey '<space>'<CR>", opts)
+map("n", "<localleader>", ":WhichKey ','<CR>", opts)
+map("v", "<leader>", ":WhichKeyVisual '<space>'<CR>", opts)
+map("v", "<localleader>", ":WhichKeyVisual ','<CR>", opts)
+
+---- Buffers
+--------------
+which_key_map.n = { name = "+buffers" }
 map("n", "<leader>bn", ":bnext<cr>", opts)
 map("n", "<leader>bp", ":bprev<cr>", opts)
 map("n", "<leader>bd", ":bdelete<cr>", opts)
 map("n", "<leader><TAB>", "<C-^>", opts) -- switch to last buffer
 
 
--- Project
-------------
+---- Project
+--------------
+which_key_map.p = { name = "+project" }
 map("n", "<leader>pt", ":NERDTreeToggleVCS<CR>", opts) -- open project tree
 map("n", "<leader>po", ":NERDTreeFind<CR>", opts) -- reveal current file in project
 map("n", "<leader>pf", ":Files<CR>", opts) -- find in pwd
 
--- Files
-----------
+
+---- Files
+------------
+which_key_map.f = { name = "+files" }
 map("n", "<leader>ft", ":NerdTreeToggle<CR>", opts) -- open files tree
 map("n", "<leader>fr", ":History<CR>", opts) -- recent files
 
--- Root
----------
+
+---- Root
+-----------
 map("n", "<leader><esc>", ":nohlsearch<cr>", opts)
+
+-- `vim.g` variables can't be modified in place from lua
+vim.g.which_key_map = which_key_map
+
