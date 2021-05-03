e18f96f0e2b0216e72678a27a5c146118118f7c4
Alexander Juda <alexanderjuda@gmail.com>
2021-05-03 18:43:16 +0200

    Add keymap for open buffers

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -17,32 +17,33 @@ map("n", "<localleader>", ":WhichKey ','<CR>", opts)
 map("v", "<leader>", ":WhichKeyVisual '<space>'<CR>", opts)
 map("v", "<localleader>", ":WhichKeyVisual ','<CR>", opts)
 
----- Buffers
---------------
+-- Buffers
+------------
 which_key_map.n = { name = "+buffers" }
+map("n", "<leader>bb", ":Buffers<cr>", opts)
 map("n", "<leader>bn", ":bnext<cr>", opts)
 map("n", "<leader>bp", ":bprev<cr>", opts)
 map("n", "<leader>bd", ":bdelete<cr>", opts)
 map("n", "<leader><TAB>", "<C-^>", opts) -- switch to last buffer
 
 
----- Project
---------------
+-- Project
+------------
 which_key_map.p = { name = "+project" }
 map("n", "<leader>pt", ":NERDTreeToggleVCS<CR>", opts) -- open project tree
 map("n", "<leader>po", ":NERDTreeFind<CR>", opts) -- reveal current file in project
 map("n", "<leader>pf", ":Files<CR>", opts) -- find in pwd
 
 
----- Files
-------------
+-- Files
+----------
 which_key_map.f = { name = "+files" }
 map("n", "<leader>ft", ":NerdTreeToggle<CR>", opts) -- open files tree
 map("n", "<leader>fr", ":History<CR>", opts) -- recent files
 
 
----- Root
------------
+-- Root
+---------
 map("n", "<leader><esc>", ":nohlsearch<cr>", opts)
 
 -- `vim.g` variables can't be modified in place from lua
