27ab4a4d2e79d4c79c2c213b530a4b27dcec44c9
alexjuda2 <alexanderjuda+2@gmail.com>
2021-06-27 13:41:45 +0200

    Hide tilde for empty lines

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -47,6 +47,9 @@ vim.wo.number = true
 vim.o.numberwidth = 6
 vim.wo.numberwidth = 6
 
+-- Don't show tildes for end of buffer lines
+vim.o.fillchars = vim.o.fillchars .. "eob: "
+
 -- Enable scrolling with mouse
 vim.o.mouse = "a"
 
