525dda339339469bd19633d661caf22b11d47b5b
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-31 20:19:35 +0200

    Fix closing vim when just closing a buffer

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -113,7 +113,8 @@ augroup aj-nerdtree-buffers
     autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
 
     " Close the tab if NERDTree is the only window remaining in it.
-    autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
+    " Note: this does more harm than good.
+    " autocmd BufEnter * if winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
 
     " If another buffer tries to replace NERDTree, put it in the other window, and bring back NERDTree.
     autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 | let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
