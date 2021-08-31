e944e4e270bb97526724c7eacca157d01976443d
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-31 20:21:25 +0200

    Fix formatting for paq deps

--- .config/nvim/lua/pkgs.lua
+++ .config/nvim/lua/pkgs.lua
@@ -3,76 +3,76 @@
 ---------------
 
 require "paq" {
-"savq/paq-nvim";                  -- Let Paq manage itself
+    "savq/paq-nvim";                  -- Let Paq manage itself
 
---------------
--- Packages
---------------
+    --------------
+    -- Packages
+    --------------
 
--- Theme 
-----------
-{'tjdevries/colorbuddy.vim'};
-{'Th3Whit3Wolf/onebuddy'};
-
-
--- UI
--------
--- file explorer
-{ 'scrooloose/nerdtree' };
--- leader keybindings helper window
-{ 'liuchengxu/vim-which-key' };
--- `cd` after opening a file
-{ 'airblade/vim-rooter' };
--- code parser for syntax highlight & folds
-{'nvim-treesitter/nvim-treesitter', run=function() vim.cmd(":TSUpdate") end}; -- Update the parsers on plugin update
-
--- buffers presented in a tab bar + fancy icons
-{'akinsho/nvim-bufferline.lua'};
-{'kyazdani42/nvim-web-devicons'};
-
--- nicer bottom status line
-{'itchyny/lightline.vim'};
+    -- Theme 
+    ----------
+    'tjdevries/colorbuddy.vim';
+    'Th3Whit3Wolf/onebuddy';
 
 
--- LSP
--- -----
-{'neovim/nvim-lspconfig'};
+    -- UI
+    -------
+    -- file explorer
+    'scrooloose/nerdtree';
+    -- leader keybindings helper window
+    'liuchengxu/vim-which-key';
+    -- `cd` after opening a file
+    'airblade/vim-rooter';
+    -- code parser for syntax highlight & folds
+    {'nvim-treesitter/nvim-treesitter', run=function() vim.cmd(":TSUpdate") end}; -- Update the parsers on plugin update
+
+    -- buffers presented in a tab bar + fancy icons
+    'akinsho/nvim-bufferline.lua';
+    'kyazdani42/nvim-web-devicons';
+
+    -- nicer bottom status line
+    'itchyny/lightline.vim';
 
 
--- Fuzzy completion
----------------------
-{'junegunn/fzf', run=vim.fn["fzf#install"]};
-{'junegunn/fzf.vim'};
-
--- fuzzy matching for LSP completion
-{'nvim-lua/completion-nvim'};
-
--- fuzzy matching for other LSP commands
-{'ojroques/nvim-lspfuzzy'};
+    -- LSP
+    -- -----
+    'neovim/nvim-lspconfig';
 
 
--- Editing
-------------
--- trigger comments with `gcc`
-{'tpope/vim-commentary'};
--- wrap selected block in chars
-'machakann/vim-sandwich';
+    -- Fuzzy completion
+    ---------------------
+    {'junegunn/fzf', run=vim.fn["fzf#install"]};
+    'junegunn/fzf.vim';
 
--- Languages
---------------
--- Nix 
-'LnL7/vim-nix';
+    -- fuzzy matching for LSP completion
+    'nvim-lua/completion-nvim';
+
+    -- fuzzy matching for other LSP commands
+    'ojroques/nvim-lspfuzzy';
 
 
--- Integrations
------------------
-'jupyter-vim/jupyter-vim';
+    -- Editing
+    ------------
+    -- trigger comments with `gcc`
+    'tpope/vim-commentary';
+    -- wrap selected block in chars
+    'machakann/vim-sandwich';
 
-----------
--- Notes
-----------
-'tpope/vim-markdown';
--- Plug 'iamcco/markdown-preview.nvim', { 'do': { -> mkdp#util#install() }, 'for': ['markdown', 'vim-plug']}
-'jbyuki/nabla.nvim';
+    -- Languages
+    --------------
+    -- Nix 
+    'LnL7/vim-nix';
+
+
+    -- Integrations
+    -----------------
+    'jupyter-vim/jupyter-vim';
+
+    ----------
+    -- Notes
+    ----------
+    'tpope/vim-markdown';
+    -- Plug 'iamcco/markdown-preview.nvim', { 'do': { -> mkdp#util#install() }, 'for': ['markdown', 'vim-plug']}
+    'jbyuki/nabla.nvim';
 
 }
