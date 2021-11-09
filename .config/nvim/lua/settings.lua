93bbe5ae5600fb1a5cea6212aaf837eb459247eb
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-11-09 12:07:51 +0100

    Add textobjects for fn call

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -190,8 +190,10 @@ require"nvim-treesitter.configs".setup {
         -- You can use the capture groups defined in textobjects.scm
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
+        ["ac"] = "@call.outer",
+        ["ic"] = "@call.inner",
+        ["ak"] = "@class.outer",
+        ["ik"] = "@class.inner",
       },
     },
   },
