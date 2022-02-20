09bb494e40ee32792bfd0b8620ffb0d4dbec1ec6
Alexander Juda <alexanderjuda@gmail.com>
2022-02-20 21:14:43 +0100

    Remove unused incremental selection

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -171,16 +171,6 @@ require"nvim-treesitter.configs".setup {
     enable = true,
   },
 
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
-    },
-  },
-
   indent = {
     enable = false,
   },
