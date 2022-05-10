8652333fed4aa7d2e9f9dfacc0ff49bdfc0c44c5
Alexander Juda <alexander.juda@zapatacomputing.com>
2022-05-10 11:34:06 +0200

    Add keymap for closing pane

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,10 +6,15 @@ wezterm.on("update-right-status", function(window, pane)
 end)
 
 return {
+    -- Override shell. System bash in macOS os old. Let's use the homebrew one.
     default_prog = {"/usr/local/bin/bash", "-l",},
+
+    -- Custom keybindings
     keys = {
         {key="9", mods="ALT", action=wezterm.action{ShowLauncherArgs={flags="FUZZY|WORKSPACES"}}},
         {key="n", mods="ALT", action=wezterm.action{SwitchWorkspaceRelative=1}},
         {key="p", mods="ALT", action=wezterm.action{SwitchWorkspaceRelative=-1}},
+        {key="w", mods="ALT|CMD", action=wezterm.action{CloseCurrentPane={confirm=true}}},
+        {key="w", mods="CTRL|SHIFT|ALT", action=wezterm.action{CloseCurrentPane={confirm=true}}},
   },
 }
