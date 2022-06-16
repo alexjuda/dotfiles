bc8eff791a89eba981eba1d6ffa93abd7d46ad03
Alexander Juda <alexanderjuda@gmail.com>
2022-06-16 09:09:34 +0200

    Hide neovim's bottom command line

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -79,8 +79,11 @@ vim.cmd[[language en_US.UTF-8]]
 
 
 -- Use a single, unified status line regardless of the number of windows. Works
--- with neovim>=0.7 (edge atm).
+-- with neovim>=0.7.
 vim.o.laststatus = 3
+
+-- Hide command line if not currently writing a command. Works with neovim >=0.8.
+vim.o.cmdheight = 0
 
 ---------------
 -- which-key --
