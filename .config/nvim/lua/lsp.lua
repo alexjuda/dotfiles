cf4cc5ad9370930825981191111ace7c24af93de
Alexander Juda <alexanderjuda@gmail.com>
2022-07-17 08:30:18 +0200

    Print warning when dict file doesn't exist

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -191,6 +191,14 @@ local function read_dict_words()
     -- Workaround for ltex-ls not reading/writing dict files.
 
     local path = vim.env.HOME .. "/.local/share/lang-servers/ltex-ls-data/dict.txt"
+
+    local f = io.open(path)
+    if f==nil then
+        print("Warning: dict file doesn't exist at" .. path)
+        return {}
+    end
+    io.close(f)
+
     local lines = {}
     for line in io.lines(path) do
         lines[#lines + 1] = line
