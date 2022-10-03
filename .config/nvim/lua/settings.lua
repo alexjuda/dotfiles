80d1f33fa73357812e34ae5164d04a8a0a0eb778
Alexander Juda <alexander.juda@zapatacomputing.com>
2022-10-03 13:29:26 +0200

    Workaround setting font colors

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -164,7 +164,7 @@ augroup END
 -- vim.g.tokyonight_style = "night"
 -- vim.cmd[[colorscheme tokyonight]]
 
-vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme tokyonight]])
 vim.cmd([[colorscheme gruvbox]])
 
 ----------------
