39f4ef372a93af59baa52c0054b6a38c4fb4698c
Alexander Juda <alexander.juda@zapatacomputing.com>
2023-03-10 12:01:22 +0100

    Auto-change terminal color based on system light/dark mode

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,18 +8,26 @@ local on_linux = function()
     return output == "Linux\n"
 end
 
+local function scheme_for_appearance(appearance)
+    -- Based on https://wezfurlong.org/wezterm/config/lua/wezterm.gui/get_appearance.html
+    if appearance:find 'Dark' then
+        -- return 'Builtin Solarized Dark'
+        return 'ayu'
+    else
+        return 'ayu_light'
+    end
+end
+
 return {
     -- System bash in macOS os old. Let's use the homebrew one if not on Linux.
-    default_prog = on_linux() and {"/bin/bash", "-l",} or {"/usr/local/bin/bash", "-l",},
-
-    color_scheme = "ayu",
-
+    default_prog = on_linux() and { "/bin/bash", "-l", } or { "/usr/local/bin/bash", "-l", },
+    -- Determine colorscheme based on System-reported light/dark appearance.
+    color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
     -- Custom keybindings
     keys = {
-        {key="w", mods="ALT|CMD", action=wezterm.action{CloseCurrentPane={confirm=true}}},
-        {key="w", mods="CTRL|SHIFT|ALT", action=wezterm.action{CloseCurrentPane={confirm=true}}},
+        { key = "w", mods = "ALT|CMD",    action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+        { key = "w", mods = "CTRL|SHIFT|ALT", action = wezterm.action { CloseCurrentPane = { confirm = true } } },
     },
-
     -- Borders
     window_padding = {
         left = 0,
@@ -27,13 +35,11 @@ return {
         top = 0,
         bottom = 0,
     },
-
     -- Fonts
     -- The default font is the bundled JetBrains Mono. Some time ago it lacked the nerd glyphs required by nvim-tree, but nowadays it seems this was fixed.
 
     -- Change font size without messing up OS window layout.
     adjust_window_size_when_changing_font_size = false,
-
     -- The default font size (10.0) is too small on 4K screens.
     font_size = 14,
 }
