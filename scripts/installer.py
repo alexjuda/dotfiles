32217aed73a981358b413cac4f73a6dc6503e25f
Alexander Juda <alexanderjuda@gmail.com>
2023-04-28 18:44:06 +0200

    Ask and run

--- scripts/installer.py
+++ scripts/installer.py
@@ -3,14 +3,32 @@
 import subprocess
 
 
+class ColorCode:
+    # The terminal codes usually include brackets. Having a single opening
+    # bracket screws up code editors.
+    BOLD = "\033" + chr(91) + "1m"
+    END = "\033" + chr(91) + "0m"
+
+
 def _run_cmd(cmd: str):
     proc = subprocess.run(cmd, shell=True)
     proc.check_returncode()
 
 
-def main():
-    _run_cmd("ls -al")
+def _print_bold(text: str):
+    print(ColorCode.BOLD + text + ColorCode.END)
 
+
+def _ask_run_cmd(cmd: str):
+    _print_bold(cmd)
+    ans = input("Run this command? [y]es/[n]o/[q]uit ")
+
+    if ans == "y":
+        _run_cmd(cmd)
+
+
+def main():
+    _ask_run_cmd("ls -al")
 
 
 if __name__ == "__main__":
