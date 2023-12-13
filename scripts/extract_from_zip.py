696b89070040e8e8e74c7865771dacda79705ad3
Alexander Juda <alexanderjuda@gmail.com>
2023-12-14 00:00:46 +0100

    Fix redirects

--- scripts/extract_from_zip.py
+++ scripts/extract_from_zip.py
@@ -8,6 +8,7 @@ from tempfile import NamedTemporaryFile
 from argparse import ArgumentParser
 from pathlib import Path
 from http.client import HTTPSConnection
+from zipfile import ZipFile
 
 
 logger = logging.getLogger(__name__)
@@ -29,17 +30,35 @@ def main():
 
 def _extract(url, file_glob, target_dir):
     parsed = urlparse(url)
-    resp = _send_get(host=parsed.hostname, path=parsed.path, headers={})
+    resp = _send_get(host=parsed.hostname, path=parsed.path, headers={"User-Agent": "python"})
     with NamedTemporaryFile() as tmp_file:
-        n_bytes = resp.readinto(tmp_file)
-        logger.info(f"Fetched {n_bytes} bytes into {tmp_file.name}")
+        while chunk := resp.read(1000):
+            tmp_file.write(chunk)
+        logger.info(f"Fetched {tmp_file.name}")
         tmp_file.seek(0)
+
+        zip_file = ZipFile(tmp_file)
+        logger.info(f"Files inside the zip: {zip_file.namelist()}")
+
+
+def _parse_url_comps(url):
+    parsed = urlparse(url)
+    host = parsed.netloc
+    scheme_host = f"{parsed.scheme}://{host}"
+    return host, url[len(scheme_host):]
 
 
 def _send_get(host, path, headers):
     conn = HTTPSConnection(host)
     conn.request("GET", path, headers=headers)
     resp = conn.getresponse()
+
+    if resp.status == 302:
+        # Follow redirects until the call stack is filled
+        new_url = resp.headers["Location"]
+        return _send_get(*_parse_url_comps(new_url), headers)
+
+
     if resp.status not in {200, 201}:
         raise RuntimeError(f"Bad status code: {resp.status}")
 
