40b904480555c9c97543927bd9b3766e8e03f7b5
alexjuda <alexanderjuda@gmail.com>
2023-12-22 23:45:13 +0100

    Add pyenv palybook

--- macos/pyenv.sh
+++ macos/pyenv.sh
@@ -1,4 +1,4 @@
 # Keeping it in another file helps loading it lazily.
-export PYENV_ROOT="$HOME/.pyenv"
+export PYENV_ROOT="$HOME/.local/share/pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
