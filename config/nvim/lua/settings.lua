08b47bc4a26ef0940c7f4525756fba001f2b9085
alexjuda <alexanderjuda@gmail.com>
2024-04-11 23:02:50 +0200

    Fix spam from octo.nvim and gh and missing scopes

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -119,7 +119,11 @@ require("nvim-search-and-replace").setup()
 require('image').setup {}
 
 
-require("octo").setup {}
+require("octo").setup {
+  suppress_missing_scope = {
+    projects_v2 = true,
+  }
+}
 
 ------------
 -- Colors --
