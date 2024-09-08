ccd3c87cd30a0410382349d2cf7d6fd8c8c31a2d
alexjuda <alexanderjuda@gmail.com>
2024-09-08 23:06:41 +0200

    Fix deprecated TS LSP name

--- config/nvim/lua/aj/lsp/js.lua
+++ config/nvim/lua/aj/lsp/js.lua
@@ -7,7 +7,7 @@ M.setup = function()
     -- Requires `typescript` and `typescript-language-server` packages.
     -- If they're not installed locally, it will fetch them each time.
 
-    require("lspconfig").tsserver.setup {
+    require("lspconfig").ts_ls.setup {
         cmd = { "npx", "typescript-language-server", "--stdio", },
         on_attach = common.shared_on_attach,
         capabilities = common.make_shared_capabilities(),
