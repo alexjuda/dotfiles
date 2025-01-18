e677893d37c8e003bde3578efd6e2013a2b1116c
alexjuda <alexanderjuda@gmail.com>
2025-01-18 11:38:02 +0100

    More coherent lualine separators

--- config/nvim/lua/aj/pkg_settings.lua
+++ config/nvim/lua/aj/pkg_settings.lua
@@ -228,6 +228,10 @@ local setup_all = function()
         end
 
         require("lualine").setup {
+            options = {
+                component_separators = { left = "│", right = "│" },
+                section_separators = { left = "", right = "", },
+            },
             sections = {
                 lualine_a = {
                     {
