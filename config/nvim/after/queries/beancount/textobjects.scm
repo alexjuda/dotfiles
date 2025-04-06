5ef8e43aad58921877846239c76b0170a2522af1
alexjuda <alexanderjuda@gmail.com>
2025-04-06 11:41:08 +0200

    Enable navigating over beancount files

---
+++ config/nvim/after/queries/beancount/textobjects.scm
@@ -0,0 +1,4 @@
+;; Allows navigating between sections using Treesitter Text Objects.
+;;
+;; Unfortunately, `@class.inner` would only capture individual section children instead of the section body as a whole.
+(section) @class.outer
