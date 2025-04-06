312b016298fbfdc9294384058979fb323d76a757
alexjuda <alexanderjuda@gmail.com>
2025-04-06 18:51:04 +0200

    Show individual transactions

--- config/nvim/after/queries/beancount/aerial.scm
+++ config/nvim/after/queries/beancount/aerial.scm
@@ -3,3 +3,10 @@
   headline: (headline
     item: (item) @name
     (#set! "kind" "Class"))) @symbol
+
+;; This could be used for showing individual transactions. I'd have to adjust the aerial filters for this to work.
+(transaction
+  date: (date)
+  txn: (txn)
+  narration: (narration) @name
+  (#set! "kind" "Variable")) @symbol
