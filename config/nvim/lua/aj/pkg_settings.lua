53988ba0c541e4ec32fd11e39ef597c144f73129
alexjuda <alexanderjuda@gmail.com>
2025-04-12 20:56:50 +0200

    Less distracting markdown headings and code blocks

--- config/nvim/lua/aj/pkg_settings.lua
+++ config/nvim/lua/aj/pkg_settings.lua
@@ -481,10 +481,25 @@ local setup_all = function()
     ------------------------------
 
     local markdown = function()
-        -- Markdown --
-        --------------
         require("mdeval").setup {
             require_confirmation = false,
+        }
+
+        require("render-markdown").setup {
+            heading = {
+                -- By default, headings would get a full-line bar.
+                width = "block",
+                left_pad = 1,
+                right_pad = 1,
+                border = true,
+                -- Make all the headings symmetric in shape.
+                position = "inline",
+            },
+            code = {
+                -- Defaults to concealing the bottom ```. Switching between insert and normal modes causes the whole
+                -- page to flicker, which is super annoying.
+                border = "thin",
+            },
         }
     end
     markdown()
