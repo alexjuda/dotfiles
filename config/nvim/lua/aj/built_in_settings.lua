75ddec3ba6674bdf171496abd4d1b85b40e4af36
alexjuda <alexanderjuda@gmail.com>
2025-07-26 14:35:25 +0200

    Use default cmdheight

--- config/nvim/lua/aj/built_in_settings.lua
+++ config/nvim/lua/aj/built_in_settings.lua
@@ -90,9 +90,6 @@ M.setup = function()
     -- Set langauge
     vim.cmd [[language en_US.UTF-8]]
 
-    -- Hide command line if not currently writing a command. Works with neovim >=0.8.
-    vim.o.cmdheight = 0
-
     -- Show horizontal line at cursor row.
     vim.opt.cursorline = true
 
