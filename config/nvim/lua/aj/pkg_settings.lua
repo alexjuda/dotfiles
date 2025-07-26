2766a3ced03a647f205f906c7fdd82231541b336
alexjuda <alexanderjuda@gmail.com>
2025-07-26 14:53:42 +0200

    Update neotree config

--- config/nvim/lua/aj/pkg_settings.lua
+++ config/nvim/lua/aj/pkg_settings.lua
@@ -369,19 +369,6 @@ local setup_all = function()
 
     local neo_tree = function()
         -- Project File Tree
-        -- Unless you are still migrating, remove the deprecated commands from v1.x
-        vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
-        -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-        vim.fn.sign_define("DiagnosticSignError",
-            { text = " ", texthl = "DiagnosticSignError" })
-        vim.fn.sign_define("DiagnosticSignWarn",
-            { text = " ", texthl = "DiagnosticSignWarn" })
-        vim.fn.sign_define("DiagnosticSignInfo",
-            { text = " ", texthl = "DiagnosticSignInfo" })
-        vim.fn.sign_define("DiagnosticSignHint",
-            { text = "", texthl = "DiagnosticSignHint" })
-
         require("neo-tree").setup {
             filesystem = {
                 -- Let dirbuf handle editing directories.
