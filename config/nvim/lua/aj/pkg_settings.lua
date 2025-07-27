e0efe1a9f783407a4013ff75713dc1126e2e7f53
alexjuda <alexanderjuda@gmail.com>
2025-07-27 19:28:12 +0200

    Improve diagram rendering on dark mode

--- config/nvim/lua/aj/pkg_settings.lua
+++ config/nvim/lua/aj/pkg_settings.lua
@@ -493,6 +493,9 @@ local setup_all = function()
                 mermaid = {
                     -- Default scale is 1, but it's way too small for my monitors.
                     scale = 4,
+                    -- Default settings work good for light mode, but I use dark mode more.
+                    theme = "dark",
+                    background = "transparent",
                 },
             },
         }
