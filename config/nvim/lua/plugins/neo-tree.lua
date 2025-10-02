c93f3743e8115f3cc6f7361c9e5f073f26a44bbb
alexjuda <alexanderjuda@gmail.com>
2025-10-02 10:45:58 +0200

    Add lazy-loaded neo-tree

---
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  }
+}
