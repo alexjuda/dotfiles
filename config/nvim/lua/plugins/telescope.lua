9841af4a937888db2cb3faad9adf9bd11cf0d2a0
alexjuda <alexanderjuda@gmail.com>
2025-10-12 21:48:38 +0200

    feat(nvim): use all available screen estate for telescope pickers

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,16 @@
 return {
     "nvim-telescope/telescope.nvim",
     lazy = true,
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+        defaults = {
+            layout_strategy = "vertical",
+            layout_config = {
+                vertical = {
+                    height = 0.99,
+                    width = 0.99,
+                },
+            },
+        },
+    },
 }
