d4c247a3e599050fc90745eeacd55d257d215fa5
alexjuda <alexanderjuda@gmail.com>
2025-12-20 17:40:26 +0100

    fix(nvim): always show hidden files in oil

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,10 @@
 return {
     "stevearc/oil.nvim",
-    opts = {},
+    opts = {
+        view_options = {
+            show_hidden = true,
+        },
+    },
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
