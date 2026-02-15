0e657ece22701dc3412a72685792f4d6a4be36c3
alexjuda <alexanderjuda@gmail.com>
2026-02-15 22:31:39 +0100

    feat(nvim): switch colorscheme to ayu with adjustments

--- config/nvim/lua/plugins/themes.lua
+++ config/nvim/lua/plugins/themes.lua
@@ -1,16 +1,26 @@
 return {
     {
-        "catppuccin/nvim",
-        name = "catppuccin",
+        "Shatur/neovim-ayu",
         -- This is my main colorscheme. Need to always load it.
         lazy = false,
         config = function()
-            vim.cmd.colorscheme "catppuccin"
+            local colors = require("ayu.colors")
+            local mirage = true
+            colors.generate(mirage)
+
+            -- Adjust gutter colors:
+            -- 1. Make the full cursor line have the same background, even on the gutter.
+            -- 2. Tone down fold markers. I don't use it often.
+            local cursor_line_bg = { bg = colors.line }
+            local line_nr_fg = { fg = colors.guide_normal }
+            require("ayu").setup {
+                overrides = {
+                    CursorLineFold = cursor_line_bg,
+                    CursorLineSign = cursor_line_bg,
+                    FoldColumn = line_nr_fg
+                },
+            }
+            vim.cmd.colorscheme "ayu-mirage"
         end
-    },
-    {
-        "rose-pine/nvim",
-        lazy = true,
-        name = "rose-pine",
     },
 }
