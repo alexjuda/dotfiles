bde93a3c2248ca914bc0320266fe588c48f2e8d2
alexjuda <alexanderjuda@gmail.com>
2026-06-21 22:02:33 +0200

    fix(nvim): always enable TS context

--- config/nvim/lua/plugins/treesitter-context.lua
+++ config/nvim/lua/plugins/treesitter-context.lua
@@ -1,10 +1,10 @@
 return {
     "nvim-treesitter/nvim-treesitter-context",
-    lazy = true,
+    lazy = false,
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     opts = {
         -- The plugin is _enabled_ by default but not shown because we're only lazy loading it on toggle.
-        enable = false,
+        enable = true,
         -- Defaults to "cursor" which is weird.
         mode = "topline",
     },
